ce2d0e4813d97a57484a47587795848a2c5d8bd7
Hidran Arias <hidran.arias@spryker.com>
2024-10-11 00:26:20 +0200

    Creates todoSlice and shows todos on main app

---
+++ src/features/todos/todoSlice.js
@@ -0,0 +1,45 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = [
+    {
+        completed: false,
+        name: 'Study react',
+        user_id:1,
+        id: (new Date()).getTime()
+    },
+    {
+        completed: true,
+        name: 'Study C#',
+        user_id: 1,
+        id: Math.random()
+    }
+];
+const todoSlice = createSlice({
+    name:'todos',
+    initialState,
+    reducers:{
+        // action.type
+        //action.payload 
+        addTodo(state, action){
+            console.log(action)
+            state.push(action.payload)
+        },
+        removeTodo(state, action) {
+
+            state = state.filter(todo => todo.id !== action.payload.id);
+
+        },
+        toggleTodo(state, action) {
+            state = state.map(todo => {
+                if (todo.id === action.payload.id){
+                    todo.completed = !todo.completed;
+                }
+                return todo;
+            });
+
+        }
+    }
+});
+export const {addTodo, removeTodo, toggleTodo} = todoSlice.actions;
+
+export default todoSlice.reducer;
