93dfb4243fbc57770d7b4a8cc4e316ac6ea869c9
Hidran Arias <hidran.arias@spryker.com>
2024-10-27 12:08:32 +0100

    Adds edit and view buttons to the list item

--- src/features/lists/ListItem.js
+++ src/features/lists/ListItem.js
@@ -9,6 +9,9 @@ function ListItem({ list }) {
     const handleRemove = () => {
         dispatch(removeList({ id: list.id }));
     };
+    const handleEdit = () => {
+        //dispatch(removeList({ id: list.id }));
+    };
     const navigate = useNavigate();
     const handleNavigate = () =>{
         navigate(`/lists/${list.id}?listName=${encodeURIComponent(list.name)}`)
@@ -19,9 +22,17 @@ function ListItem({ list }) {
                 <strong>{list.name}</strong> <br />
                 <small>Created at: {new Date(list.created_at).toLocaleString()}</small>
             </div>
-            <button className="btn btn-danger btn-sm" onClick={handleRemove}>
-                <i className="bi bi-trash"></i> {/* Bootstrap icon */}
+            <div className= "d-flex ms-3 align-items-center">
+                <button className="btn btn-primary me-3" title="Edit list" onClick={handleEdit}>
+                    <i className="bi bi-pencil-square"></i> 
+                </button>
+                <button className="btn btn-success  me-3" title ="View todos" onClick={handleNavigate}>
+                    <i className="bi bi-info-square"></i> 
+                </button>
+            <button className="btn btn-danger" title="remove lists" onClick={handleRemove}>
+                <i className="bi bi-trash"></i> 
             </button>
+            </div>
         </li>
     );
 }
--- src/features/todos/TodoList.js
+++ src/features/todos/TodoList.js
@@ -3,13 +3,14 @@ import TodoItem from './TodoItem';
 
 const TodoList = ({ listId }) =>{
   
-    const todos = useSelector(state => state.todos.filter(todo => todo.list_id === listId));
+    const todos = useSelector(state => state.todos);
+    const litsTodos = todos.filter(todo => todo.list_id === listId);
     
 return (
    
         <ul className='list-group'>
             {
-                todos.map(todo =>  <TodoItem key={todo.id} todo={todo} />)
+            litsTodos.map(todo =>  <TodoItem key={todo.id} todo={todo} />)
             }
         </ul>    
 )
